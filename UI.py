192dbef555a6d1ea04ecfcf98196997669ae1d05
Kevin Lin <kevin.lin@realtek.com>
2020-08-06 15:28:12 +0800

    fix multiselection bug

--- UI.py
+++ UI.py
@@ -42,7 +42,7 @@ class Ui_MainWindow(object):
         self.treeView.setSizePolicy(sizePolicy)
         self.treeView.setObjectName("treeView")
         self.listView = QtWidgets.QListView(self.splitter)
-        self.listView.setSelectionMode(QtWidgets.QAbstractItemView.MultiSelection)
+        self.listView.setSelectionMode(QtWidgets.QAbstractItemView.SingleSelection)
         self.listView.setObjectName("listView")
         self.imageLabel = QtWidgets.QLabel(self.splitter_2)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
