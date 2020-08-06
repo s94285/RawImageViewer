4d9e5251520c52ac4ab78544d650476ac9bc229b
Kevin Lin <kevin.lin@realtek.com>
2020-08-06 15:09:02 +0800

    finish basic functions

--- helloworld.py
+++ helloworld.py
@@ -1,7 +1,8 @@
 from PyQt5 import QtWidgets
 from UI import Ui_MainWindow
-from PyQt5.QtGui import QImage,QPixmap
+from PyQt5.QtGui import QImage,QPixmap,QStandardItemModel
 from PyQt5.QtCore import QByteArray,QFile
+from PyQt5.QtCore import Qt,QDir,QItemSelectionModel
 import sys
 
 
@@ -10,23 +11,62 @@ class MainWindow(QtWidgets.QMainWindow):
         super(MainWindow, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.ui.label.setText('Hello World!')
-        self.ui.pushButton.clicked.connect(self.showImage)
 
         self.W = 1920
         self.H = 1080
         self.IMAGESIZE = self.W*self.H*8
         self.pic = None
-        
-    def showImage(self, mainWindow):
+        self.pixmap = None
+        self.dirModel = QtWidgets.QFileSystemModel(self)
+        self.fileModel = QtWidgets.QFileSystemModel(self)
+
+        self.ui.actionTest.triggered.connect(self.test)
+        self.ui.actionOpen_Folder.triggered.connect(self.openDir)
+        self.ui.treeView.clicked.connect(self.treeViewClicked)
+
+    def openDir(self,MainWindow):
+        "Open folder selecting dialog"
+        dialog = QtWidgets.QFileDialog(self)
+        path = dialog.getExistingDirectory(None)
+        self.dirModel.setRootPath("/")
+        self.dirModel.setFilter(QDir.NoDotAndDotDot|QDir.AllDirs)
+        self.ui.treeView.setModel(self.dirModel)
+        self.ui.treeView.setRootIndex(self.dirModel.index(path))
+        self.fileModel.setRootPath(path)
+        self.ui.listView.setModel(self.fileModel)
+        self.ui.listView.selectionModel().currentChanged.connect(self.listViewMoved)
+
+    def treeViewClicked(self,index):
+        path = self.dirModel.fileInfo(index).absoluteFilePath()
+        print(path)
+        self.ui.listView.setRootIndex(self.fileModel.index(path))
+
+    def listViewMoved(self,index):
+        path = self.fileModel.fileInfo(index).absoluteFilePath()
+        print(path)
+        self.showImage(path)
+
+    def resizeEvent(self, event):
+        "Override the resize event"
+        #resize image label
+        if(self.pixmap):
+            self.ui.imageLabel.setPixmap(QPixmap(""))
+        QtWidgets.QMainWindow.resizeEvent(self, event)
+
+    def showImage(self, path):
         "Show image on graphics view"
-        print("AAA")
-        # self.ui.imageLabel
-        # picArray =
-        picFile = QFile("images/2K_good_0804_1_12.raw")
+        picFile = QFile(path)
         if(not picFile.open(QFile.ReadOnly)):return
         self.pic = QImage(picFile.read(self.IMAGESIZE),self.W,self.H,QImage.Format_Grayscale8)
-        self.ui.imageLabel.setPixmap(QPixmap.fromImage(self.pic))
+        self.pixmap = QPixmap.fromImage(self.pic)
+        self.ui.imageLabel.setPixmap(self.pixmap.scaled(self.ui.imageLabel.width(),self.ui.imageLabel.height(),Qt.KeepAspectRatio))
+
+    def test(self,MainWindow):
+        print(self.ui.listView.selectionModel())
+
+        
+        
+        
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication([])
